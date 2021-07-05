d8187a62678e2359084f968e6cf70843c1ed7fbe
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-05 10:59:45 +0200

    Use explicit types for networks
    fixes https://github.com/litentry/litentry-app/issues/159

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -3,17 +3,22 @@ import { Event } from "@polkadot/types/interfaces";
 
 export type InterestedEvent = { pattern: string, getPushData: (event: Event) => admin.messaging.TopicMessage };
 
-const kusamaNetwork = {
+export type ChainConfig = {
+  ws: string;
+  events: readonly InterestedEvent[]
+}
+
+const kusamaNetwork: ChainConfig = {
   ws: "wss://kusama.api.onfinality.io/public-ws",
-  events: [] as InterestedEvent []
-} as const;
+  events: [],
+} as const
 
-const polkadotNetwork = {
+const polkadotNetwork: ChainConfig = {
   ws: "wss://rpc.polkadot.io",
-  events: [] as InterestedEvent[]
-} as const;
+  events: [],
+} as const
 
-const litentryNetwork = {
+const litentryNetwork: ChainConfig = {
   // ws: 'wss://3.0.201.137',
   ws: "wss://staging.registrar.litentry.io",
   events: [{
@@ -28,10 +33,9 @@ const litentryNetwork = {
         }
       };
     }
-  }] as InterestedEvent[]
-} as const;
+  }]
+}
 
-export type ChainConfig = typeof kusamaNetwork | typeof polkadotNetwork | typeof litentryNetwork;
 
 export default {
   kusama: kusamaNetwork,
