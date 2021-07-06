188e76c0dc7eff5db3fd022757cca7087303ef57
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-06 14:00:48 +0200

    Move push events to their own file and address PR issue
    fixes https://github.com/litentry/litentry-app/issues/164

--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,57 +1,28 @@
-import * as admin from "firebase-admin";
-import { Event } from "@polkadot/types/interfaces";
-
-export type InterestedEvent = { pattern: string, getPushData: (event: Event) => admin.messaging.TopicMessage };
+import { getPushEvents, PushEvent } from 'chain/pushEvents';
 
 export type ChainConfig = {
   ws: string;
-  events: readonly InterestedEvent[]
-}
+  pushEvents: PushEvent[];
+};
 
 const kusamaNetwork: ChainConfig = {
-  ws: "wss://kusama.api.onfinality.io/public-ws",
-  events: [],
-} as const
+  ws: 'wss://kusama.api.onfinality.io/public-ws',
+  pushEvents: getPushEvents({ networkKey: 'kusama' }),
+} as const;
 
 const polkadotNetwork: ChainConfig = {
-  ws: "wss://rpc.polkadot.io",
-  events: [],
-} as const
+  ws: 'wss://rpc.polkadot.io',
+  pushEvents: getPushEvents({ networkKey: 'polkadot' }),
+} as const;
 
 const litentryNetwork: ChainConfig = {
   // ws: 'wss://3.0.201.137',
-  ws: "wss://staging.registrar.litentry.io",
-  events: [{
-    pattern: "treasury.Proposed",
-    getPushData() {
-      return {
-        topic: "treasury.Proposed",
-        data: { deeplink: "litentry://api/litentry_test/treasury" },
-        notification: {
-          title: `New Treasury Proposal`,
-          body: "A new treasury proposal has been submitted, check it out!"
-        }
-      };
-    }
-  }, {
-    pattern: "tips.newTip",
-    getPushData() {
-      return {
-        topic: "tips.newTip",
-        data: { deeplink: "litentry://api/litentry_test/tips" },
-        notification: {
-          title: "Tip Suggestion",
-          body: "A new tip has been suggested, check it out!"
-        }
-      };
-    }
-  }]
-}
-
+  ws: 'wss://staging.registrar.litentry.io',
+  pushEvents: getPushEvents({ networkKey: 'litentry_test' }),
+};
 
 export default {
   kusama: kusamaNetwork,
   polkadot: polkadotNetwork,
-  litentry_test: litentryNetwork
+  litentry_test: litentryNetwork,
 };
-
