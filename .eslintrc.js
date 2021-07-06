4a7132dbc4b39c98bdffad1d9ed3dc381b778441
Amir Hoseinian <hoseinian.amir@gmail.com>
2021-07-06 13:40:30 +0200

    Update eslint config for new eslint-config-prettier
    more info on : https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,6 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    // "plugin:@typescript-eslint/recommended",
-    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'prettier/@typescript-eslint',
     // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:prettier/recommended',
   ],
