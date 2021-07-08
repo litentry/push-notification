7db9d75141d4d5d7eec11f0a16acad3ff05f4a68
Francisco Silva <franjs.francisco@gmail.com>
2021-07-08 18:39:45 +0200

    creating api module, initial steps for creating subscriptions

---
+++ src/api/index.ts
@@ -0,0 +1,240 @@
+import _ from 'lodash';
+import express from 'express';
+import { body, validationResult } from 'express-validator';
+import { PrismaClient } from '@prisma/client';
+// import logger from '../logger';
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  return res.json({ msg: 'Hello API' });
+});
+
+router.post(
+  '/subscriptions',
+  body('deviceToken').notEmpty().isString(),
+  body('walletAddress').notEmpty().isString(),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { deviceToken, walletAddress } = req.body;
+
+    let subscription = await prisma.subscription.findUnique({
+      where: {
+        deviceToken: deviceToken,
+      },
+      include: {
+        walletAddresses: true,
+      },
+    });
+
+    if (!subscription) {
+      subscription = await prisma.subscription.create({
+        data: {
+          deviceToken,
+          // TODO: include events??
+          walletAddresses: {
+            create: [{ address: walletAddress }],
+          },
+        },
+        include: {
+          walletAddresses: true,
+        },
+      });
+    }
+
+    if (!subscription.walletAddresses.some((wallet) => wallet.address === walletAddress)) {
+      const updatedSubscription = await prisma.subscription.update({
+        where: {
+          id: subscription.id,
+        },
+        data: {
+          walletAddresses: {
+            create: [{ address: walletAddress }],
+          },
+        },
+        include: {
+          walletAddresses: true,
+        },
+      });
+      subscription = updatedSubscription;
+    }
+
+    res.json(subscription);
+  }
+);
+
+// /**
+//  * @description A user register from our APP
+//  * @param {string} deviceToken - the device token of user's device (e.g. phone)
+//  * @param {string} walletAddress - the walletAddress of a user
+//  */
+// router.post('/register', async (req: Request, res: Response) => {
+//   try {
+//     const { deviceToken, walletAddress } = req.body;
+//     logger.debug(`device token is: ${deviceToken}, walletAddress is: ${walletAddress}, ${walletAddress}`);
+//     if (_.isEmpty(deviceToken) || _.isEmpty(walletAddress)) {
+//       throw new Error(`deviceToken or walletAddress is missing`);
+//     }
+//     // let account = await DBHandler.Account.findOne({ deviceToken: deviceToken, walletAddress: walletAddress });
+//     // if (!account) {
+//     //   account = new Account(deviceToken, walletAddress, []);
+//     //   await DBHandler.Account.persist(account).flush();
+//     // }
+//
+//     // return res.json({ status: 'success', msg: '', account: account });
+//     return res.json({ status: 'success' });
+//   } catch (error) {
+//     logger.error(`POST /register: unexcepected error occurs`);
+//     console.trace(error);
+//
+//     res.status(400);
+//     return res.json({ status: 'fail', msg: new String(error) });
+//   }
+// });
+//
+// /**
+//  * @description A user de-register from our APP
+//  * @param {string} deviceToken - the device token of user's device (e.g. phone)
+//  * @param {string} walletAddress - the walletAddress of a user
+//  */
+// router.post('/deregister', async (req: Request, res: Response) => {
+//   try {
+//     const { deviceToken, walletAddress } = req.body;
+//     logger.debug(`device token is: ${deviceToken}, walletAddress is: ${walletAddress}`);
+//     if (_.isEmpty(deviceToken) || _.isEmpty(walletAddress)) {
+//       throw new Error(`deviceToken or walletAddress is missing`);
+//     }
+//
+//     // const account = (await DBHandler.Account.findOne({
+//     //   deviceToken: deviceToken,
+//     //   walletAddress: walletAddress,
+//     // })) as Account;
+//     // await DBHandler.Account.removeAndFlush(account);
+//     res.status(204);
+//     return res.json({ status: 'success', msg: '' });
+//   } catch (error) {
+//     logger.error(`POST /deregister: unexcepected error occurs`);
+//     console.trace(error);
+//
+//     res.status(400);
+//     return res.json({ status: 'fail', msg: new String(error) });
+//   }
+// });
+//
+// /**
+//  * @description Create a list of events subscribed by a user
+//  * @param {string} deviceToken - the device token of user's device (e.g. phone)
+//  * @param {string} walletAddress - the wallet walletAddress of a user
+//  * @param {string[]} events - a list of events a user interest.
+//  */
+// router.post('/events/config', async (req: Request, res: Response) => {
+//   try {
+//     const { deviceToken, walletAddress, events = [] } = req.body;
+//     if (_.isEmpty(events)) {
+//       return res.json({ status: 'success', msg: 'No events provided' });
+//     }
+//
+//     // let account = (await DBHandler.Account.findOne({
+//     //   deviceToken: deviceToken,
+//     //   walletAddress: walletAddress,
+//     // })) as Account;
+//     // wrap(account).assign({ events: events });
+//     // await DBHandler.Account.persist(account).flush();
+//
+//     // return res.json({ status: 'success', msg: '', account: account });
+//     return res.json({ status: 'success', msg: '' });
+//   } catch (error) {
+//     logger.error(`POST /events/config: unexcepected error occurs`);
+//     console.trace(error);
+//
+//     res.status(400);
+//     return res.json({ status: 'fail', msg: new String(error) });
+//   }
+// });
+//
+// /**
+//  * @description Update a list of events subscribed by a user
+//  * @param {string} deviceToken - the device token of user's device (e.g. phone)
+//  * @param {string} walletAddress - the wallet walletAddress of a user
+//  * @param {string[]} events - a list of events a user is interested.
+//  */
+// router.patch('/events/config', async (req: Request, res: Response) => {
+//   try {
+//     const { deviceToken, walletAddress, events = [] } = req.body;
+//     if (_.isEmpty(events)) {
+//       return res.json({ status: 'success', msg: 'No events provided' });
+//     }
+//
+//     // let account = (await DBHandler.Account.findOne({
+//     //   deviceToken: deviceToken,
+//     //   walletAddress: walletAddress,
+//     // })) as Account;
+//     // Update events
+//     // let newEvents = new Set(account.events);
+//
+//     //     for (let e of events) {
+//     //       if (!newEvents.has(e)) {
+//     //         newEvents.add(e);
+//     //       }
+//     //     }
+//     //
+//     //     wrap(account).assign({ events: [...newEvents] });
+//     //     await DBHandler.Account.persist(account).flush();
+//     //
+//     //     return res.json({ status: 'success', msg: '', account: account });
+//     return res.json({ status: 'success', msg: '' });
+//   } catch (error) {
+//     logger.error(`PATCH /events/config: unexcepected error occurs`);
+//     console.trace(error);
+//
+//     res.status(400);
+//     return res.json({ status: 'fail', msg: new String(error) });
+//   }
+// });
+//
+// /**
+//  * @description remove a list of events a user is not interested in.
+//  * @param {string} deviceToken - the device token of user's device (e.g. phone)
+//  * @param {string} walletAddress - the wallet walletAddress of a user
+//  * @param {string[]} events - a list of events a user interest.
+//  */
+// router.delete('/events/config', async (req: Request, res: Response) => {
+//   try {
+//     const { deviceToken, walletAddress, events = [] } = req.body;
+//     if (_.isEmpty(events)) {
+//       return res.json({ status: 'success', msg: 'No events provided' });
+//     }
+//
+//     //     let account = (await DBHandler.Account.findOne({
+//     //       deviceToken: deviceToken,
+//     //       walletAddress: walletAddress,
+//     //     })) as Account;
+//     //     let newEvents = new Set(account.events);
+//     //
+//     //     for (let e of events) {
+//     //       if (newEvents.has(e)) {
+//     //         newEvents.delete(e);
+//     //       }
+//     //     }
+//     //
+//     //     wrap(account).assign({ events: [...newEvents] });
+//     //     await DBHandler.Account.persist(account).flush();
+//
+//     // return res.json({ status: 'success', msg: '', account: account });
+//     return res.json({ status: 'success', msg: '' });
+//   } catch (error) {
+//     logger.error(`DELETE /events/config: unexcepected error occurs`);
+//     console.trace(error);
+//
+//     res.status(400);
+//     return res.json({ status: 'fail', msg: new String(error) });
+//   }
+// });
+
+export default router;
