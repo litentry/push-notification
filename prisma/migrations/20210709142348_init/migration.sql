571e1fa1a3e57a43ac4110507cb24ed794103bd4
Francisco Silva <franjs.francisco@gmail.com>
2021-07-09 16:24:59 +0200

    updating prisma schema, reset initial migration

--- prisma/migrations/20210709142348_init/migration.sql
+++ prisma/migrations/20210709142348_init/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE "subscriptions" (
     "id" SERIAL NOT NULL,
     "device_token" VARCHAR(255) NOT NULL,
     "events" TEXT[],
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     PRIMARY KEY ("id")
 );
@@ -13,10 +13,13 @@ CREATE TABLE "subscriptions" (
 CREATE TABLE "wallet_addresses" (
     "id" SERIAL NOT NULL,
     "address" TEXT NOT NULL,
-    "subscriptionId" INTEGER NOT NULL,
+    "subscription_id" INTEGER NOT NULL,
 
     PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions.device_token_unique" ON "subscriptions"("device_token");
 
 -- CreateIndex
 CREATE INDEX "subscriptions.device_token_index" ON "subscriptions"("device_token");
@@ -25,4 +28,4 @@ CREATE INDEX "subscriptions.device_token_index" ON "subscriptions"("device_token
 CREATE INDEX "wallet_addresses.address_index" ON "wallet_addresses"("address");
 
 -- AddForeignKey
-ALTER TABLE "wallet_addresses" ADD FOREIGN KEY ("subscriptionId") REFERENCES "subscriptions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "wallet_addresses" ADD FOREIGN KEY ("subscription_id") REFERENCES "subscriptions"("id") ON DELETE CASCADE ON UPDATE CASCADE;
